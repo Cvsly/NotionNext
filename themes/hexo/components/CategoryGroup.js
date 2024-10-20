552a1f9411a3a503f492943345e0d50281bc0e1d
crushllys <106291155+Cvsly@users.noreply.github.com>
2024-10-21 01:10:21 +0800

    Update CategoryGroup.js

--- themes/hexo/components/CategoryGroup.js
+++ themes/hexo/components/CategoryGroup.js
@@ -7,18 +7,18 @@ const CategoryGroup = ({ currentCategory, categories }) => {
   return <>
     <div id='category-list' className='dark:border-gray-600 flex flex-wrap  mx-4'>
       {categories.map(category => {
-        const selected = currentCategory === category.name_cn
+        const selected = currentCategory === category.name
         return (
           <Link
-            key={category.name_cn}
-            href={`/category/${category.name_cn}`}
+            key={category.name}
+            href={`/category/${category.name}`}
             passHref
             className={(selected
               ? 'hover:text-white dark:hover:text-white bg-indigo-600 text-white '
               : 'dark:text-gray-400 text-gray-500 hover:text-white dark:hover:text-white hover:bg-indigo-600') +
               '  text-sm w-full items-center duration-300 px-2  cursor-pointer py-1 font-light'}>
 
-            <div> <i className={`mr-2 fas ${selected ? 'fa-folder-open' : 'fa-folder'}`} />{category.name_cn}({category.count})</div>
+            <div> <i className={`mr-2 fas ${selected ? 'fa-folder-open' : 'fa-folder'}`} />{category.name}({category.count})</div>
 
           </Link>
         );
