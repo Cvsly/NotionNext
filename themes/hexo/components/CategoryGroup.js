b85bf6cc234e162ac616de8dcf9dad36d551369b
crushllys <106291155+Cvsly@users.noreply.github.com>
2024-10-21 00:57:58 +0800

    Update CategoryGroup.js

--- themes/hexo/components/CategoryGroup.js
+++ themes/hexo/components/CategoryGroup.js
@@ -18,7 +18,7 @@ const CategoryGroup = ({ currentCategory, categories }) => {
               : 'dark:text-gray-400 text-gray-500 hover:text-white dark:hover:text-white hover:bg-indigo-600') +
               '  text-sm w-full items-center duration-300 px-2  cursor-pointer py-1 font-light'}>
 
-            <div> <i className={`mr-2 fas ${selected ? 'fa-folder-open' : 'fa-folder'}`} />{category.name}({category.count})</div>
+            <div> <i className={`mr-2 fas ${selected ? 'fa-folder-open' : 'fa-folder'}`} />{category.name_cn}({category.count})</div>
 
           </Link>
         );
